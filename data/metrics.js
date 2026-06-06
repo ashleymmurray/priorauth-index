cc1f0a62265e23296235cf845ad81fb812e8f262
ashleymmurray <ashley@bridgechart.com>
2026-06-06 18:45:23 -0400

    Update metrics.js

--- data/metrics.js
+++ data/metrics.js
@@ -1,5 +1,58 @@
-export const MA_2024 = [];
-
-export const ACA_2024 = [];
-
-export const CY2025_DATA = [];
+export const MA_2024 = [
+  {
+    insurer: "UnitedHealthcare",
+    denialRate: 12.8,
+    approvalRate: 87.2,
+    appealOverturnRate: 80.7,
+    sourceUrl:
+      "https://www.kff.org/medicare/medicare-advantage-insurers-made-nearly-53-million-prior-authorization-determinations-in-2024/",
+  },
+  {
+    insurer: "Centene",
+    denialRate: 12.3,
+    approvalRate: 87.7,
+    appealOverturnRate: 93.6,
+    sourceUrl:
+      "https://www.kff.org/medicare/medicare-advantage-insurers-made-nearly-53-million-prior-authorization-determinations-in-2024/",
+  },
+  {
+    insurer: "Aetna (CVS Health)",
+    denialRate: 11.9,
+    approvalRate: 88.1,
+    appealOverturnRate: 80.7,
+    sourceUrl:
+      "https://www.kff.org/medicare/medicare-advantage-insurers-made-nearly-53-million-prior-authorization-determinations-in-2024/",
+  },
+  {
+    insurer: "CVS Health (all contracts)",
+    denialRate: 10.5,
+    approvalRate: 89.5,
+    appealOverturnRate: 89.7,
+    sourceUrl:
+      "https://www.kiplinger.com/retirement/medicare/medicare-advantage-plans-prior-authorization-denial-rates",
+  },
+  {
+    insurer: "Kaiser Permanente",
+    denialRate: 6.0,
+    approvalRate: 94.0,
+    appealOverturnRate: 80.7,
+    sourceUrl:
+      "https://muni.health/blog/insurance-denial-rate-by-company-2026",
+  },
+  {
+    insurer: "Humana",
+    denialRate: 5.8,
+    approvalRate: 94.2,
+    appealOverturnRate: 80.7,
+    sourceUrl:
+      "https://www.kff.org/medicare/medicare-advantage-insurers-made-nearly-53-million-prior-authorization-determinations-in-2024/",
+  },
+  {
+    insurer: "Anthem / Elevance Health",
+    denialRate: 4.2,
+    approvalRate: 95.8,
+    appealOverturnRate: 80.7,
+    sourceUrl:
+      "https://www.kff.org/medicare/medicare-advantage-insurers-made-nearly-53-million-prior-authorization-determinations-in-2024/",
+  },
+];
